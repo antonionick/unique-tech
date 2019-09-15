b338c4538ff8cbb0afbbd3c7f0381ea997714d87
Anton Golovko <golovko639@gmail.com>
2019-09-15 15:56:35 +0300

    feat: create responsive header 480px and 768px

--- dev/blocks/header/header.js
+++ dev/blocks/header/header.js
@@ -1,4 +1,5 @@
 document.querySelector(".header__bar").addEventListener("click", openNav);
+document.querySelector(".header__slider").addEventListener("click", pickSliderItem);
 
 function openNav(e) {
   let item = e.target,
@@ -16,4 +17,16 @@ function openNav(e) {
   logo.classList.toggle("header__logo_open");
   nav.classList.toggle("header__nav_open");
   item.classList.toggle("header__bar-icon_open");
+}
+
+function pickSliderItem(e) {
+  let item = e.target,
+    parent = item.parentElement;
+
+  if (!item.classList.contains("list__item-slider")) {
+    return;
+  }
+
+  parent.querySelector(".list__item-slider_active").classList.remove("list__item-slider_active");
+  item.classList.add("list__item-slider_active");
 }
